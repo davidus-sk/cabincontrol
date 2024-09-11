d8a5f28f2d3f81f39be4d68caa6ddc32ddfca736
davidus-sk <travel@davidus.sk>
2024-09-10 21:39:41 -0400

    Create test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+$module = new WSModbusModule('192.168.1.200', 4196);
+
+for ($i=1; $i<=8; $i++) {
+  $module->setRelayState($i, true);
+  sleep(1);
+}
+
+for ($i=1; $i<=8; $i++) {
+  $module->setRelayState($i, false);
+  sleep(1);
+}
+
+unset($module);
